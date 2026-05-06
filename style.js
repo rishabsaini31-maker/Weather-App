2448522cbfc0f2bd11e49840afb8768c808a6db0
Rishabsaini <rishabsainiupw165@gmail.com>
2026-05-06 23:52:23 +0530

    Update style.js

---
+++ style.js
@@ -0,0 +1,30 @@
+body {
+
+
+  font-family: Arial;
+  background: #0f172a;
+  color: white;
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+}
+
+.container {
+  text-align: center;
+}
+
+input {
+  padding: 10px;
+  margin-right: 10px;
+}
+
+button {
+  padding: 10px;
+  background: #00ffcc;
+  border: none;
+  cursor: pointer;
+}
+
+.weather-box {
+  margin-top: 20px;
+}
